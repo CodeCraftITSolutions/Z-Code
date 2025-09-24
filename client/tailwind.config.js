be62b79ca51507866b76740251ac1722e033aed4
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2025-09-25 01:11:08 +0530

    Frontend updated

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
-      keyframes: {
-        "border-spin": {
-          "0%": { transform: "rotate(0deg)" },
-          "100%": { transform: "rotate(360deg)" },
-        },
+      keyframes:{
+        'border-spin':{
+          '100%':{
+            transform:'rotate(-360deg)',
+          
+          }
+        }
       },
-      animation: {
-        "border-spin": "border-spin 1s linear infinite",
-      },
+      animation:{
+        'border-spin':'border-spin 7s linear infinite'
+      }
+     
+    },
+    fontFamily:{
+      signature:["Great Vibes"],
     },
   },
   plugins: [],
-};
+}
