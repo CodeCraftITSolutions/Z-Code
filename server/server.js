f9819541a20b056a5e423a56c9f5bdb67709b922
razilahamed <razilahamed.mf@gmail.com>
2025-09-25 22:53:22 +0530

    cors adjusted

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = require("./app")
 const server = http.createServer(app);
 
 const allowedOrigins = process.env.NODE_ENV === 'production'
-  ? ['https://zee-code-3234074b267f.herokuapp.com']
+  ? ['https://z-code-server.onrender.com']
   : ['http://localhost:5173', 'https://zee-code-3234074b267f.herokuapp.com']; // Add other local origins if necessary
 
 
